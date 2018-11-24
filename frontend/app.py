3514f5caf8ecd3c6d6dfadf6b20acca6e5759d95
lsafina <safinalrr@gmail.com>
2018-11-25 00:01:25 +0200

    charts on drugs

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from flask import Flask
 from flask import Markup
 from flask import Flask
@@ -10,8 +11,14 @@ app = Flask(__name__)
 def chart():
     labels = random.sample(range(1, 100), 10)
     values = random.sample(range(1, 100), 10)
+
+    color = ["#e8ee3a","#403aee","#403aee","#e8ee3a", "#f49080","#e8ee3a","#403aee","#e8ee3a", "#f49080","#403aee"]
+
+    message = ["Foo","","Bar","","","","","Noooo", "",""]
+
+    (lab,val,mes,col) = zip(*sorted(zip(labels, values, message, color), key=lambda x: x[0]))
     
-    return render_template('index.html', values=values, labels=labels)
+    return render_template('index.html', values=val, labels=lab, messages=mes, colors=col)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)                       
